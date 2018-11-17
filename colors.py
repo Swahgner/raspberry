0f0ea41275ae59cf5589a960b8a1f4c3c44f28fa
Swahgner <swahgnerdk@gmail.com>
2018-11-17 15:12:29 +0100

    Update colors.py

--- colors.py
+++ colors.py
@@ -3,4 +3,12 @@ from random import randint
 import time
 
 sense = SenseHat()
-sense.low_light = True
+
+
+while True:
+  x = randint(0,8)
+  y = randint(0,8)
+  
+  color = [randint(50,255), randint(50,255), randint(50,255)]
+  
+  sense.set_pixel(x,y,color)

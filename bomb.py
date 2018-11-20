bdfba1dededaab3e5e2c4f7f529dfc25dde18ebf
Swahgner <swahgnerdk@gmail.com>
2018-11-20 20:59:32 +0100

    Create bomb.py

---
+++ bomb.py
@@ -0,0 +1,22 @@
+from sense_hat import SenseHat
+import time
+from random import randint
+
+sense = SenseHat()
+sense.clear()
+
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+yellow = (255,255,0)
+
+
+
+for x in range(8):
+  for y in range(8):
+    sense.set_pixel(x,y,yellow)
+
